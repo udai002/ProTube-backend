dc762a30727dc667c4e6e635d09b45ecb71c4b29
udai sai <karmuriudaisai002@gmail.com>
2024-09-23 14:09:35 +0530

    added status middleware

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const connectMongo = async ()=>{
 connectMongo()
 
 app.use(express.json())
-
+app.use(express.static('public'))
 
 app.get('/' , (req , res)=>{
     res.send({message:"Welcome to express app"})
