5622d7712f77eb50dfbf166fdf2d7d3ea0ea3c37
udai sai <karmuriudaisai002@gmail.com>
2024-09-27 12:26:52 +0530

    added cros origin

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ connectMongo()
 app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/' , (req , res)=>{
     res.send({message:"Welcome to express app"})
